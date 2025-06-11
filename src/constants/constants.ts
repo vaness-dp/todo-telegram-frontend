8d7490d0064cd6772c522f14cd236e962bbf543d
krylov <vaness.engineer@gmail.com>
2025-06-11 23:26:33 +0300

    refactor: constants api url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,4 @@
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || ''
-export const API_URL = 'http://localhost:5001/api'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
+	? `${process.env.NEXT_PUBLIC_API_URL}/api`
+	: 'http://localhost:5001/api'
