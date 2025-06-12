21aaf82f9a530ddf9fd0c8fd3c5a3562d1b2455b
krylov <vaness.engineer@gmail.com>
2025-06-13 02:00:24 +0300

    chore: update next.js config, add custom headers and optimizations

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
-	swcMinify: true,
 	env: {
 		TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN
 	},
@@ -14,7 +13,16 @@ const nextConfig: NextConfig = {
 					{
 						key: 'Content-Security-Policy',
 						value:
-							"default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://telegram.org https://*.telegram.org; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://*.telegram.org; connect-src 'self' https://*.telegram.org wss://*.telegram.org;"
+							"default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://telegram.org https://*.telegram.org; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: https://*.telegram.org; connect-src 'self' https://*.telegram.org wss://*.telegram.org https://todo-backend-go7y6hw5f-vaness-dps-projects.vercel.app;"
+					}
+				]
+			},
+			{
+				source: '/_next/static/:path*',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 'public, max-age=31536000, immutable'
 					}
 				]
 			}
