a96f037ca77bf83241f29bdbe9da2e2e2d9b96db
krylov <vaness.engineer@gmail.com>
2025-06-13 01:58:09 +0300

    chore: configure axios instance with baseURL, headers, and keep-alive agents

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,5 +1,7 @@
 import type { CreateAxiosDefaults } from 'axios'
 import axios from 'axios'
+import { Agent as HttpAgent } from 'http'
+import { Agent as HttpsAgent } from 'https'
 
 import { API_URL } from '@/constants/constants'
 
@@ -8,11 +10,10 @@ const options: CreateAxiosDefaults = {
 	headers: {
 		'Content-Type': 'application/json'
 	},
-	// Оптимизация запросов
 	timeout: 10000, // 10 секунд таймаут
 	timeoutErrorMessage: 'Request timeout',
-	httpAgent: new (require('http').Agent)({ keepAlive: true }),
-	httpsAgent: new (require('https').Agent)({ keepAlive: true })
+	httpAgent: new HttpAgent({ keepAlive: true }),
+	httpsAgent: new HttpsAgent({ keepAlive: true })
 }
 
 export const axiosClassic = axios.create(options)
